3ab67f5a93ac76f2b90322a07f26d14fd21ed9d8
shamsjafri21 <shams.jafri@teknikality.co.uk>
2021-09-06 23:23:12 -0700

    first commit

---
+++ src/main/java/com/example/GckExample/GckExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.GckExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GckExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GckExampleApplication.class, args);
+	}
+
+}
